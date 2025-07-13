58e1abfdfde397445c4b90059ba502a184a3244c
Keks <keks@htmlacademy.ru>
2025-07-13 12:50:53 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/function.js
@@ -0,0 +1,40 @@
+// Задача #1
+
+
+function checkStringLenght (string, maxLength) {
+  if (string.lenght <= maxLength) {
+    return true;
+  }
+}
+
+// Задача #2
+
+function palindromeCheck (string = '') {
+  string = string.toString();
+  string = string.toLowerCase();
+  string = string.replaceAll(" ", "");
+
+  let reversedString = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+
+  return string === reversedString;
+}
+
+// Задача #3
+
+function extractNumbers (string) {
+  let result = '';
+
+  string = string.toString();
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (!Number.isNaN(parseInt(string[i], 10))) {
+      result += string[i];
+    }
+  }
+
+  return parseInt(result, 10);
+}
